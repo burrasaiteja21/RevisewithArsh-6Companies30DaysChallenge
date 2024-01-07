3ec7089efeecb97681e77a7b714d934f15370f6d
burrasaiteja21 <burra.saiteja21@gmail.com>
2024-01-07 22:51:23 +0530

    day2

--- NumberofWaystoReachaPositionAfterExactlykSteps.java
+++ NumberofWaystoReachaPositionAfterExactlykSteps.java
@@ -1,13 +1,8 @@
 //leetcode link:https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
 class Solution {
-    int[][] dp;
-    public int numberOfWays(int startPos, int endPos, int k)
+     public int numberOfWays(int startPos, int endPos, int k)
     {
-        dp=new int[4001][1001];
-        for(int[] ar:dp)
-        {
-            Arrays.fill(ar,-1);
-        }
+
         return numways(startPos,endPos,k);
 
     }
@@ -21,13 +16,9 @@ class Solution {
         {
             return 0;
         }
-        if(dp[start+2001][k]!=-1)
-        {
-            return dp[start+2001][k];
-        }
-        int take=numways(start-1,end,k-1);
+         int take=numways(start-1,end,k-1);
         int notake=numways(start+1,end,k-1);
-        dp[start+2001][k]=(take+notake)%1000000007;
-        return dp[start+2001][k];
+        return (take+notake)%1000000007;
+        
     }
 }
